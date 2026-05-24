ee8114b6f889966a800c3956bfbce1f3f24760bb
nicky_sus <gfmm2558@gmail.com>
2026-05-24 10:50:04 +0300

    Add ApplyFriction method

--- src/physics.cs
+++ src/physics.cs
@@ -3,4 +3,11 @@
 public static class Physics
 {
     public static double ApplyGravity(double velocity, double gravity, double deltaTime) => velocity + gravity * deltaTime;
+    public static double ApplyFriction(double velocity, double friction, double deltaTime)
+    {
+        double v = friction * Math.Abs(velocity) * deltaTime;
+        if (v >= Math.Abs(velocity))
+            return 0.0f;
+        return velocity - Math.Sign(velocity) * v;
+    }
 }
