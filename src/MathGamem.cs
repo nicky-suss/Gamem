0e73eaa58b60aec950070dd3e8d4d668445c215c
nicky_sus <gfmm2558@gmail.com>
2026-05-24 11:20:34 +0300

    Add SmoothStep method

--- src/MathGamem.cs
+++ src/MathGamem.cs
@@ -2,5 +2,16 @@ namespace Gamem;
 
 public static class MathGamem
 {
-    
+    public static double SmoothStep(double start, double end, double t)
+    {
+        double c = Math.Clamp(t, 0.0, 1.0);
+        double T = c * c * (3.0 - 2.0 * c);
+        return start + (end - start) * T;
+    }
+    public static float SmoothStep(float start, float end, float t)
+    {
+        float c = Math.Clamp(t, 0.0f, 1.0f);
+        float T = c * c * (3.0f - 2.0f * c);
+        return start + (end - start) * T;
+    }
 }
