b10b6572a84beaf6af91c24a2b6bc5c72e90ff9a
nicky_sus <gfmm2558@gmail.com>
2026-07-07 20:11:24 +0300

    Add MathGm methods

--- src/Web/src/WasmLoader.ts
+++ src/Web/src/WasmLoader.ts
@@ -1,5 +1,20 @@
 export interface GamemWasmModule {
-    //
+    // === MathGm ===
+    _gamem_smooth_step(start: number, end: number, t: number): number;
+    _gamem_random_range(min: number, max: number): number;
+    _gamem_lerp(start: number, end: number, t: number): number;
+    _gamem_lerp_unclamped(start: number, end: number, t: number): number;
+    _gamem_inverse_lerp(value: number, start: number, end: number): number;
+    _gamem_accelerate(Vcurrent: number, Vtarget: number, a: number, t: number): number;
+    _gamem_map(toMin: number, v: number, fromMin: number, toMax: number, fromMax: number): number;
+    _gamem_remap(toMin: number, v: number, fromMin: number, toMax: number, fromMax: number): number;
+    _gamem_roll_chance(chance: number): number;
+    _gamem_move_towards(current: number, target: number, speed: number, dt: number): number;
+    _gamem_safe_divide(a: number, b: number): number;
+    _gamem_safe_divide_fb(a: number, b: number, fallback: number): number;
+    _gamem_approximately(a: number, b: number): number;
+    _gamem_smooth_damp(current: number, target: number, currentVelocity: number, smoothTime: number, maxSpeed: number, deltaTim: number): number;
+    _gamem_smooth_damp_angle(current: number, target: number, currentVelocity: number, smoothTime: number, deltaTime: number): number;
 }
 let wasmModule: GamemWasmModule | null = null;
 
